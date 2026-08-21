c9e22242818a3a6c61f9e214880ee1786d8fed2f
Alsya Kanaya Dzikra <157259452+alsyakd@users.noreply.github.com>
2026-08-21 22:00:01 +0700

    initial commit

---
+++ identitas.py
@@ -0,0 +1,11 @@
+nama = "Alsya Kanaya Dzikra"
+nim = 2605060075
+ttl = "Magelang, 12 Oktober 2007"
+jk = "Perempuan"
+hobi = "Coding & Gaming"
+
+print("Nama :", nama)
+print("NIM :", nim)
+print("Tempat Tanggal Lahir :", ttl)
+print("Jenis Kelamin :", jk)
+print("Hobi :", hobi)
